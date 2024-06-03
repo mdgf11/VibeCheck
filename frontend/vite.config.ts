17255c0cf44f61760257f496f717cae79b35b390
mdgf11 <miguel.dias.fonseca@gmail.com>
2024-06-03 20:42:30 +0100

    vercel you are monster from hell

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import dotenv from 'dotenv'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import dotenv from 'dotenv';
 
 // Load the appropriate .env file based on the mode
-dotenv.config({ path: `.env.${process.env.VITE_APP_NODE_ENV}` })
+dotenv.config({ path: `.env.${process.env.VITE_APP_NODE_ENV}` });
 
-const baseURL = process.env.VERCEL_URL 
+const baseURL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : process.env.VITE_APP_BASE_URL;
 
-const port = process.env.VITE_APP_PORT ? parseInt(process.env.VITE_APP_PORT, 10) : undefined;
+const port = process.env.VITE_APP_PORT ? parseInt(process.env.VITE_APP_PORT, 10) : 3000;
 
 export default defineConfig({
   plugins: [vue()],
@@ -21,12 +21,12 @@ export default defineConfig({
     },
   },
   server: {
-    port: port || 3000,  // Use the port from .env or fallback to 3000
+    port: port, // Use the port from .env or fallback to 3000
   },
   define: {
     'process.env': {
       VITE_APP_BASE_URL: baseURL,
       VITE_APP_PORT: port,
     }
-  }
-})
+  },
+});
