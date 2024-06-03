a7b5c13ebfa0300615093569f68cbfc2e6db3aa7
mdgf11 <miguel.dias.fonseca@gmail.com>
2024-06-03 13:01:08 +0100

    updated spotifyApiController

--- frontend/src/services/SpotifyAPIController.ts
+++ frontend/src/services/SpotifyAPIController.ts
@@ -1,8 +1,7 @@
-
 const env = import.meta.env;
 
 export async function getProfile() {
-    const clientId = import.meta.env.VITE_APP_SPOTIFY_CLIENT_ID;
+    const clientId = env.VITE_APP_SPOTIFY_CLIENT_ID;
     const params = new URLSearchParams(window.location.search);
     const code = params.get("code");
 
@@ -15,51 +14,54 @@ export async function getProfile() {
     }
 }
 
-async function  redirectToAuthCodeFlow(clientId: string) {
+async function redirectToAuthCodeFlow(clientId: string) {
     const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
+    const challenge = await generateCodeChallenge(await verifier);
 
-    localStorage.setItem("verifier", verifier);
+    localStorage.setItem("verifier", await verifier);
+
+    const redirectUri = `${env.VITE_APP_BASE_URL}:${env.VITE_APP_PORT}/callback`;
 
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:8081/callback");
+    params.append("redirect_uri", redirectUri);
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
 
     window.location.href = "https://accounts.spotify.com/authorize?" + params.toString();
+}
 
-    function generateCodeVerifier(length: number) {
-        let text = '';
-        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    
-        for (let i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
+async function generateCodeVerifier(length: number) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
-    
-    async function generateCodeChallenge(codeVerifier: string) {
-        const data = new TextEncoder().encode(codeVerifier);
-        const digest = await window.crypto.subtle.digest('SHA-256', data);
-        return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/=+$/, '');
-    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
 }
 
 export async function getAccessToken(clientId: string, code: string): Promise<string> {
     const verifier = localStorage.getItem("verifier");
-    console.log(verifier);
+    const redirectUri = `${env.VITE_APP_BASE_URL}:${env.VITE_APP_PORT}/callback`;
+
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:8081/callback");
-    if (verifier!=null)
+    params.append("redirect_uri", redirectUri);
+    if (verifier != null)
         params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
@@ -71,7 +73,7 @@ export async function getAccessToken(clientId: string, code: string): Promise<st
     const { access_token } = await result.json();
     return access_token;
 }
-    
+
 async function fetchProfile(token: string): Promise<JSON> {
     const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
