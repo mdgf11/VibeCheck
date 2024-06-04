e0517d5453f3f90d9d486af665744d1905268c04
mdgf11 <miguel.dias.fonseca@gmail.com>
2024-06-04 12:20:44 +0100

    fuck me

--- backend/src/main/java/pt/migfonseca/vibecheck/VibeCheck.java
+++ backend/src/main/java/pt/migfonseca/vibecheck/VibeCheck.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VibecheckApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(VibecheckApplication.class, args);
-	}
+public class VibeCheck {
 
+    public static void main(String[] args) {
+        SpringApplication.run(VibeCheck.class, args);
+    }
 }
